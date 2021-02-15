9c00cda337c9cf1439223c67601254fc2270eefe
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-15 09:56:17 +0200

    added core Application and Router classes

---
+++ core/Application.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\core;
+/**
+ * Class Application
+ * 
+ * This is main application
+ * 
+ * @package app\core
+ */
+
+class Application
+{
+    public function __construct()
+    {
+        echo 'This is Application constructor <br>';
+    }
+
+}
+
