9e0b8dfe453f2a21662408a858c3c5adc1d08ca0
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-15 09:31:24 +0200

    index php road map

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Almost Lara';
+$app = new Application();
+
+$router = new Router();
+
+$router->get('/', function () {
+    return "this is home page";
+});
+
+$router->get('/about', function () {
+    return "this is about page";
+});
+
+$app->run();
