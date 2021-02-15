bd4fdf958c751f9ff2efa76730180267c1c1b27c
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-15 09:57:30 +0200

    tested Application constructor

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
+require_once 'core/Application.php';
+
+use app\core\Application;
 
 $app = new Application();
 
-$router = new Router();
+// $router = new Router();
 
-$router->get('/', function () {
-    return "this is home page";
-});
+// $router->get('/', function () {
+//     return "this is home page";
+// });
 
-$router->get('/about', function () {
-    return "this is about page";
-});
+// $router->get('/about', function () {
+//     return "this is about page";
+// });
 
-$app->run();
+// $app->run();
