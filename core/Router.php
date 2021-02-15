ad8a13415c1006bb5fa692c55db7d42c0976f8ef
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-15 11:54:38 +0200

    add two routes and run application

--- core/Router.php
+++ core/Router.php
@@ -11,9 +11,41 @@ namespace app\core;
 
 class Router
 {
+    /**
+     * This will hold all routes.
+     * 
+     * routes [
+     * ['get' => [
+    * ['/' => function return,],
+    * ['/about' => function return,],
+     * ]]
+     * ['/' => function return,],
+     * ['/about' => function return,],
+     * 
+     * ];
+     * 
+     * @var array
+     */
+
+
+    protected array $routes = [];
     public function __construct()
     {
         echo 'This is Router constructor <br>';
     }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+    
+    public function resolve()
+    {
+        echo "<pre>";
+        var_dump($this->routes);
+        echo "</pre>";
+        exit;
+    }
+
     
 }
