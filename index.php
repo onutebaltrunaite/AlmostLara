5676da01c8062c5cf61a039590025d2730916c01
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-15 11:53:12 +0200

    array $routes + fn get + fn resolve

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
+// phpinfo();exit;
 // require_once 'core/Application.php';
 // require_once 'core/Router.php';
 
 require_once 'vendor/autoload.php';
 
 use app\core\Application;
-use app\core\Router;
+// use app\core\Router;
 
 
 $app = new Application();
 
-$router = new Router();
 
-// $router->get('/', function () {
-//     return "this is home page";
-// });
 
-// $router->get('/about', function () {
-//     return "this is about page";
-// });
 
-// $app->run();
+$app->router->get('/', function () {
+    return "this is home page";
+});
+
+$app->router->get('/about', function () {
+    return "this is about page";
+});
+
+$app->run();
