e2aadcd7e47411ff20f7e64dae163f8b18240692
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-18 09:49:44 +0200

    bootstrap simple form for testing

---
+++ view/contact.php
@@ -0,0 +1,14 @@
+<h1>This is contact page</h1>
+
+<form action="" method="post">
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email"">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
