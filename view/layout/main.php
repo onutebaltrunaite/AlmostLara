ecbc6dfc1322f87496e2b59b8f13f0f4c46e4ca1
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-18 09:52:32 +0200

    container around the content

--- view/layout/main.php
+++ view/layout/main.php
@@ -11,7 +11,11 @@
     <title>MVC</title>
 </head>
 <body>
-{{content}}
+
+<div class="container">
+        {{content}}
+</div>
+
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
