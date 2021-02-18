2fff16e9179998ffb7f045e6a127e285b5b01988
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-18 10:39:24 +0200

    created sitecontroller and handle contact simple method

---
+++ controller/SiteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controller;
+
+
+class SiteController
+{
+    public static function handleContact()
+    {
+        return "handling form from site controller handle for method";
+    }
+
+
+}
