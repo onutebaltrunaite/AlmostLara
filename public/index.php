8e6a84bcae49866fa4f53dbe751eee64536f1c73
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-23 00:32:23 +0200

    add routes and use statement to make posts

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../vendor/autoload.php';
 
+use app\controller\PostsController;
 use app\controller\SiteController;
 use app\core\Application;
 use app\core\AuthController;
@@ -53,5 +54,8 @@ $app->router->post('/login', [AuthController::class, 'login']);
 
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
+
+$app->router->get('/posts', [PostsController::class, 'index']);
+
 
 $app->run();
