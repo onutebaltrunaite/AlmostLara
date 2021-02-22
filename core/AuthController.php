6c943d237f264a6d2499aad0263a024e1c3213c3
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-22 09:00:41 +0200

    ctreated data array and pass it down to view. register post request get data from getBody

--- core/AuthController.php
+++ core/AuthController.php
@@ -21,12 +21,38 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         if ($request->isGet()) :
-            $this->setLayout('auth');
-            return $this->render('register');
+            // $this->setLayout('auth');
+
+            $data = [
+                'name'      => '',
+                'email'     => '',
+                'password'  => '',
+                'confirmPassword' => '',
+                'errors' => [
+                    'nameErr'      => '',
+                    'emailErr'     => '',
+                    'passwordErr'  => '',
+                    'confirmPasswordErr' => '',
+                ],
+                'currentPage' => 'register',
+            ];
+
+
+
+            return $this->render('register', $data);
         endif;
 
         if ($request->isPost()) :
-            return "Validating form";
+
+            // reques s post  and we need to pull data
+            $data = $request->getBody();
+
+            // echo "<pre>";
+            // var_dump($data);
+            // echo "</pre>";
+            // exit;
+
+            return $this->render('register', $data);
         endif;
       
     }
