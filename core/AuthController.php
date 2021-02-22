e273dcab76321ee2ab7e086ad760b012050dcfd6
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-22 22:50:37 +0200

    register logic for when there are no errors

--- core/AuthController.php
+++ core/AuthController.php
@@ -4,25 +4,23 @@
 namespace app\core;
 
 /**
- * responsible for handling register and login
- * 
- * 
- * 
+ * Respossible for handling login and register.
+ *
+ * Class AuthController
+ * @package app\core
  */
 class AuthController extends Controller
 {
     public Validation $vld;
-
 
     public function __construct()
     {
         $this->vld = new Validation();
     }
 
-
     public function login()
     {
-        // have ability to change layout
+        // have ability to change laout
         $this->setLayout('auth');
         return $this->render('login');
     }
@@ -30,8 +28,9 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         if ($request->isGet()) :
-            // $this->setLayout('auth');
+//            $this->setLayout('auth');
 
+            // create data
             $data = [
                 'name'      => '',
                 'email'     => '',
@@ -53,26 +52,46 @@ class AuthController extends Controller
 
         if ($request->isPost()) :
 
-            // reques s post  and we need to pull data
+            // reques is post and we need to pull user data
             $data = $request->getBody();
-
 
             $data['errors']['nameErr'] = $this->vld->validateName($data['name']);
 
+//            $data['errors']['emailErr'] = $this->vld->validateEmail($data['email'], $this->userModel);
             $data['errors']['emailErr'] = $this->vld->validateEmail($data['email']);
-            // $data['errors']['emailErr'] = $this->vld->validateEmail($data['email'], $this->userModel);
+
             $data['errors']['passwordErr'] = $this->vld->validatePassword($data['password'], 6, 10);
 
             $data['errors']['confirmPasswordErr'] = $this->vld->confirmPassword($data['confirmPassword']);
 
+            // if there are no errors
+            if ($this->vld->ifEmptyArr($data['errors'])) :
 
-            // echo "<pre>";
-            // var_dump($data);
-            // echo "</pre>";
-            // exit;
+                // echo "<pre>";
+                // var_dumb("there r no errors");
+                // echo "</pre>";
+                // exit;
+                
+                // hash password // save way to store pass
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                // create user
+                if ($this->userModel->register($data)) {
+                    // success user added
+                    // set flash msg
+//                    flash('register_success', 'You have registered successfully');
+                    // header("Location: " . URLROOT . "/users/login");
+                    redirect('/login');
+                } else {
+                    die('Something went wrong in adding user to db');
+                }
+
+            endif;
+
+
+
 
             return $this->render('register', $data);
         endif;
-      
     }
 }
