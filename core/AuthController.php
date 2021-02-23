0e1d5cfc8c8b38d2ff50ab14bbd26ec18c6e7e6e
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-23 09:01:55 +0200

    login validation and logic imported and testing vardump

--- core/AuthController.php
+++ core/AuthController.php
@@ -23,11 +23,61 @@ class AuthController extends Controller
         $this->userModel = new UserModel();
     }
 
-    public function login()
+    public function login(Request $request)
     {
         // have ability to change laout
         $this->setLayout('auth');
-        return $this->render('login');
+        
+
+        if ($request->isGet()) :
+
+            $data = [
+                'email'     => '',
+                'password'  => '',
+                'errors' => [
+                    'emailErr'     => '',
+                    'passwordErr'  => '',
+                ]
+            ];
+            return $this->render('login', $data);
+
+        endif;
+
+        if ($request->isPost()) :
+
+            $data = $request->getBody();
+
+            $data['errors']['emailErr'] = $this->vld->validateLoginEmail($data['email'], $this->userModel);
+
+            $data['errors']['passwordErr'] = $this->vld->validateEmpty($data['password'], 'Please enter your password');
+
+                        // check if we have errors
+                        if ($this->vld->ifEmptyArr($data['errors'])) {
+                            // no errors 
+                            // email was found and password was entered
+                            $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+                            // echo "<pre>";
+                            // var_dump($loggedInUser);
+                            // echo "</pre>";
+                            // exit;
+            
+                            if ($loggedInUser) {
+                                // create session 
+                                // password match
+                                // die('email and passs match start session immediately');
+                                // $this->createUserSession($loggedInUser);
+                            } else {
+                                $data['errors']['passwordErr'] = 'Wrong password or email';
+                                // load view with errors
+                                return $this->render('users/login', $data);
+                            }
+            
+                            // die('SUCCESS');
+                        }
+
+            return $this->render('login', $data);
+        endif;
     }
 
     public function register(Request $request)
