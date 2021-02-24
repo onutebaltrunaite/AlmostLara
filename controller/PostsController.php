8c45bb287f54a6f6278bba4062f663197b12abc5
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-02-24 11:08:14 +0200

    add usermodel. use getpostId to get post, getUserById to get user

--- controller/PostsController.php
+++ controller/PostsController.php
@@ -7,14 +7,17 @@ namespace app\controller;
 use app\core\Controller;
 use app\core\Request;
 use app\model\PostModel;
+use app\model\UserModel;
 
 class PostsController extends Controller
 {
     public PostModel $postModel;
+    public UserModel $userModel;
 
     public function __construct()
     {
         $this->postModel = new PostModel();
+        $this->userModel = new UserModel();
     }
 
     public function index(Request $request)
@@ -33,10 +36,26 @@ class PostsController extends Controller
 
         if ($urlParam['value']) :
 
-            $data = [
-                $urlParam['name'] => $urlParam['value'],
 
+            $id = $urlParam['value'];
+            // get post row 
+            $post = $this->postModel->getPostById($id);
+            // lets get user data by user_id
+            $user = $this->userModel->getUserById($post->user_id);
+
+
+
+    //     echo "<pre>";
+    //    print_r($user);
+    //    echo "</pre>";
+    //    exit;
+
+            $data = [
+                // $urlParam['name'] => $urlParam['value'],
+                'post' => $post,
+                'user' => $user,
             ];
+
             return $this->render('posts/singlePost', $data);
 
         endif;
